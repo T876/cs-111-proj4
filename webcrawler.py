31cd161ec597a3eac980bca769d49e6a161f676e
kilom3ters <mwJohnson404@gmail.com>
2024-04-15 17:00:25 -0600

    updated count_links function

--- webcrawler.py
+++ webcrawler.py
@@ -1,8 +1,9 @@
 import sys
 import requests
 import bs4
-import matplotlib
+import matplotlib.pyplot as plt
 from RequestGuard import RequestGuard
+import csv
 
 # Define global variables
 args = sys.argv
@@ -12,7 +13,7 @@ request_guard = RequestGuard(args[2])
 # Parse user input
 def parse_commands(user_args):
     if user_args[1] == '-c':
-        count_links(sys.argv[2])
+        count_links(sys.argv[2], sys.argv[3], sys.argv[4])
     elif user_args[1] == '-p':
         plot_data()
     elif user_args[1] == '-i':
@@ -27,11 +28,15 @@ def parse_commands(user_args):
 
 
 # Main functions
-def count_links(url):
+def count_links(url, plot_file, csv_file):
 
     links_dict = {}
+
     def visit_link(start):
-        response = requests.get(start)
+        if request_guard.can_follow_link(start):
+            response = requests.get(start)
+        else:
+            return
         soup = bs4.BeautifulSoup(response.content, 'html.parser')
         # Build list of links to visit
         links_to_visit = []
@@ -40,14 +45,20 @@ def count_links(url):
 
         for url in links_to_visit:
             # TODO: Check if the link is forbidden, stop if it is
-            if links_dict[url]:
-                links_dict[url] += 1
-            else:
-                links_dict[url] = 1
-                visit_link(url)
+                if links_dict[url]:
+                    links_dict[url] += 1
+                else:
+                    links_dict[url] = 1
+                    visit_link(url)
     # TODO: Call helper
+    visit_link(url)
     # TODO: plot the results from our dictionary
-
+    plt.hist(links_dict)
+    plt.savefig(plot_file)
+    plt.clf()
+    with open(csv_file, 'w') as file2:
+        writer = csv.DictWriter(file2, links_dict)
+        writer.writerow(links_dict)
 
 
 def plot_data():
@@ -56,6 +67,3 @@ def plot_data():
 
 def manipulate_image(flag):
     pass
-
-
-
