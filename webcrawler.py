e1ae085dca0ebd3e7efaa8c268d0560335efd205
kilom3ters <mwJohnson404@gmail.com>
2024-04-16 12:36:36 -0600

    broke functions into pieces and cleaned code as well as added detailed comments and notes

--- webcrawler.py
+++ webcrawler.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from RequestGuard import RequestGuard
 import csv
 import re
+import numpy as np
 
 # Define global variables
 args = sys.argv
@@ -30,64 +31,108 @@ def parse_commands(user_args):
         return False
 
 
+def make_soup_obj(rg_obj, link):
+    # Uses Request Guard to verify link is not forbidden to access
+    if rg_obj.can_follow_link(link):
+        # Get the HTML and return the usable soup object
+        page = requests.get(link)
+        html = bs4.BeautifulSoup(page.text, "html.parser")
+        return html
+
+
 # Main functions
 def count_links(url, plot_file, csv_file):
-    # Construct request guard
-    domain_finder = re.compile(r'https://.*?/')
-    domain = re.match(domain_finder, url).group()
-    rg_obj = RequestGuard(domain)
-    links_to_visit = [url]
-    link_visits = {}
-
-    # prep links
-    def process_url(l9nk):
-        if l9nk.startswith('http'):
-            if '#' in l9nk:
-                parts = l9nk.split('#')
+    # Helper functions
+    def process_url(link):
+        # Get the link into the processable format
+        # Return complete links
+        if link.startswith('http'):
+            # Remove ending fragments from completed links
+            if '#' in link:
+                parts = link.split('#')
                 return parts[0]
-            return l9nk
-        if l9nk.startswith('/'):
-            return domain + l9nk[1:]
-        if l9nk.startswith('#'):
+            return link
+        # Process within a domain
+        if link.startswith('/'):
+            return domain + link[1:]
+        # Process link fragments
+        if link.startswith('#'):
             return url
         else:
             # Find the base URL for internal links
             url_match = re.compile(r'(https://.*/).*')
             final_url = re.match(url_match, args[2]).group(1)
-            return final_url + l9nk
+            return final_url + link
 
-    # make dictionary
-    for link in links_to_visit:
-        if rg_obj.can_follow_link(link):
-            page = requests.get(link)
-            html = bs4.BeautifulSoup(page.text, "html.parser")
-            for tag in html.find_all('a'):
-                href = tag.get('href')
-                to_append = process_url(href)
-                if to_append not in links_to_visit:
-                    links_to_visit.append(to_append)
+    def make_links_to_visit(soup_obj):
+        # Find all the links on a page
+        for tag in soup_obj.find_all('a'):
+            href = tag.get('href')
+            # Get the link into a usable format
+            to_append = process_url(href)
+            # Add or update the dictionary
+            update_link_visits(to_append)
+            # Add to the list of links that must be visited
+            if to_append not in links_to_visit:
+                links_to_visit.append(to_append)
 
-                if to_append not in link_visits:
-                    link_visits[to_append] = 1
-                else:
-                    link_visits[to_append] += 1
+    def update_link_visits(link):
+        # Add links to the dictionary the first time they are visited
+        if link not in link_visits:
+            link_visits[link] = 1
+        # Update the number of times links are visited each time they are visited
+        else:
+            link_visits[link] += 1
 
-    # make histogram
-    final_data = list(link_visits.values())
-    bins = range(1, max(final_data) + 2)
-    plt.clf()
-    plt.hist(final_data, bins=bins)
-    plt.savefig(plot_file)
-    plt.clf()
+    def make_hist(visits, o_plot_file):
+        # Get the number of visits information from the link_visits list
+        final_data = list(visits.values())
+        # Get the range of bins for the histogram by getting arranging the final visit counts and going from one to +2
+        # of the highest value in the list
+        bins = np.arange(1, max(final_data) + 2)
+        # Create the histogram using the created list of values and the list of bins
+        plt.clf()
+        plt.hist(final_data, bins=bins)
+        plt.savefig(o_plot_file)
+        plt.clf()
+        # Return the values used in the histogram for use in the csv
+        hist, bin_edges = np.histogram(final_data, bins=bins)
+        return hist, bin_edges
 
-    # make csv
-    with open(csv_file, "w") as file:
-        for x in bins[:-1]:
-            count = 0
-            for item in final_data:
-                if x == item:
-                    count += 1
-            file.write(f'{float(x)},{float(count)}\n')
+    def make_csv_output(hist, bin_edges, o_csv_file):
+        # Open the output file and prepare it to write to a csv file
+        with open(o_csv_file, 'w') as file:
+            writer = csv.writer(file)
+            for x in range(len(hist)):
+                # Writing the start of the bin and the count as a csv float pair
+                writer.writerow([float(bin_edges[x]), float(hist[x])])
+
+    # Main body
+    # Get the domain and prepare the request guard object
+    domain_finder = re.compile(r'https://.*?/')
+    domain = re.match(domain_finder, url).group()
+    rg_obj = RequestGuard(domain)
+    # Prepare lists and dictionary
+    links_to_visit = [url]
+    link_visits = {}
+    visited_urls = set()
+    # Initialize counter loop
+    i = 0
+    while i < len(links_to_visit):
+        # Verify what link is being processed
+        current_url = links_to_visit[i]
+        # Update lists and dictionary
+        if current_url not in visited_urls:
+            html = make_soup_obj(rg_obj, current_url)
+            if html is not None:
+                make_links_to_visit(html)
+            visited_urls.add(current_url)
+        # Increase counter for the loop
+        i += 1
+    # Make histogram and get the values for the csv files
+    histo, edges = make_hist(link_visits, plot_file)
+    # Create csv output
+    make_csv_output(histo, edges, csv_file)
 
 
 def plot_data():
